3ae1d45e48dcae0228b15f805652133f8e0c7891
quixxxs <faquijano02@gmail.com>
2025-11-02 23:54:57 -0600

    login/signup

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,32 +1,10 @@
 // src/auth/dto/auth.dto.ts
-import {
-  IsEmail,
-  IsOptional,
-  IsString,
-  Matches,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthPayloadDto {
-  @IsOptional()
   @IsEmail({})
-  correo?: string;
-
-  @IsOptional()
-  @IsString()
-  @Matches(/^(?:\d{4}-\d{4}-\d{5}|\d{13})$/, {
-    message: 'El DNI debe tener el formato ####-####-##### o 13 dígitos.',
-  })
-  dni?: string;
-
-  @IsOptional()
-  @IsString()
-  @Matches(/^(?:\+?504[-\s]?)?(?:\d{8}|\d{4}[-\s]?\d{4})$/, {
-    message:
-      'El teléfono debe tener 8 dígitos o incluir el código de país (+504).',
-  })
-  telefono?: string;
+  correo: string;
 
   @ApiProperty({ description: 'Contraseña del usuario' })
   @IsString()
