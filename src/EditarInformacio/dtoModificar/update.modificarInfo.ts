10360698a2d6a7d55f68db527bf3de2e59a41f9f
quixxxs <faquijano02@gmail.com>
2025-11-25 14:46:58 -0600

    actualizar información de usuario

--- src/EditarInformacio/dtoModificar/update.modificarInfo.ts
+++ src/EditarInformacio/dtoModificar/update.modificarInfo.ts
@@ -1,32 +1,60 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { ApiPropertyOptional, ApiPropertyOptions } from "@nestjs/swagger";
-import { IsDateString, IsOptional, IsString } from "class-validator";
-
+import { PartialType } from '@nestjs/mapped-types';
+import { ApiPropertyOptional, ApiPropertyOptions } from '@nestjs/swagger';
+import { IsDateString, IsEmail, IsOptional, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class UpdateModificarInfoDto {
-    @IsString()
-    @IsOptional()
-    nombre?: string;
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  @MaxLength(50)
+  @Matches(/^[\p{L}][\p{L}\s'’-]{1,49}$/u, {
+    message: 'El nombre solo puede contener letras, espacios y guiones.',
+  })
+  nombre?: string;
 
-    @IsString()
-    @IsOptional()
-    dni?: string
-    
-    @IsString()
-    @IsOptional()
-    telefono?: string
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  @MaxLength(50)
+  @Matches(/^[\p{L}][\p{L}\s'’-]{1,49}$/u, {
+    message: 'El apellido solo puede contener letras, espacios y guiones.',
+  })
+  apellido?: string;
 
-    @IsString()
-    @IsOptional()
-    direccion?: string;
+  @IsString()
+  @IsOptional()
+  @Matches(/^(?:\d{4}-\d{4}-\d{5}|\d{13})$/, {
+    message: 'El DNI debe tener el formato ####-####-##### o 13 dígitos.',
+  })
+  dni?: string;
 
-    @IsDateString()
-    @IsOptional()
-    fechaNac?: string;
+  @IsString()
+  @IsOptional()
+  @Matches(/^(?:\+?504[-\s]?)?(?:\d{8}|\d{4}[-\s]?\d{4})$/, {
+    message:
+      'El teléfono debe tener 8 dígitos o incluir el código de país (+504).',
+  })
+  telefono?: string;
 
-    @IsString()
-    @IsOptional()
-    password?: string
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  @MaxLength(50)
+  direccion?: string;
 
-  
+  @IsDateString()
+  @IsOptional()
+  fechaNac?: string;
+
+  @IsString()
+  @IsOptional()
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/, {
+    message:
+      'La contraseña debe incluir mayúscula, minúscula, número y carácter especial.',
+  })
+  password?: string;
+
+  @IsEmail()
+  @IsOptional()
+  correo?: string;
 }
