258cd94b82c44f317c55eb7d1e35d8175fb92371
MejiaEduardo <158190716+MejiaEduardo@users.noreply.github.com>
2025-11-26 20:24:51 -0600

    HU27 terminada con pruebas unitarias

--- src/historialFacturas/historialF.service.ts
+++ src/historialFacturas/historialF.service.ts
@@ -4,13 +4,31 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class FacturasService {
   constructor(private prisma: PrismaService) {}
-async historialFactura() {
 
-  return await this.prisma.factura.findMany({
-    include: { 
-       detalles: true
+  async historialFactura() {
+  try {
+    const facturas = await this.prisma.factura.findMany({
+      include: {
+        detalles: true,
+      },
+    });
+
+    //  Validar si no hay facturas
+    if (!facturas || facturas.length === 0) {
+      throw new NotFoundException('No existen facturas registradas.');
     }
-  });
+
+    return {
+      message: 'Historial de facturas obtenido correctamente.',
+      totalFacturas: facturas.length,
+      data: facturas,
+    };
+
+  } catch (error) {
+    console.error('Error al obtener el historial de facturas:', error);
+    throw error; // Nest manejará automáticamente el error
+  }
 }
+
 
 }
